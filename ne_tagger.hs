59553e357359a6cca03c3519d85f6b084ebbe9c7
Matthew Piziak <mjp2175@columbia.edu>
2012-09-27 16:51:45 -0400

    Converted association list to Data.Map

--- ne_tagger.hs
+++ ne_tagger.hs
@@ -2,34 +2,37 @@ import System.IO
 import System.Environment
 import Data.List
 import Data.Maybe
+import qualified Data.Map as Map
 
 --runs the program
 main = do
   args <- getArgs
   file <- readFile $ head args
   let wordtags = getWordtags file
-  mapM print $ process wordtags
+  print $ process wordtags
   
 --gets a list of WORDTAG entries from the file
 getWordtags file = filter isWordtag $ lines file
                    where isWordtag = isInfixOf "WORDTAG"
 
-process wordtags = [totalWordCount word wordtags | word <- wordList wordtags]
-               
-wordList wordtags = map getWord $ wordtags        
+process wordtags = emit "these" "O" wordtags
+                   
+emit word tag wordtags = x / y
+  where x = fromIntegral $ wordtagCount word tag wordtags
+        y = fromIntegral $ totalWordCount word wordtags
+                   
+wordList wordtags = map getWord $ wordtags
                    where getWord line = (words line)!!3
 
 totalWordCount word wordtags = sum $ map countOfTag tags
   where countOfTag tag = wordtagCount word tag wordtags
                      
-wordtagCount :: String -> String -> [String] -> Int
 wordtagCount word tag wordtags = read 
                                  $ fromMaybe "0" 
-                                 $ lookup (word, tag) 
+                                 $ Map.lookup (word, tag) 
                                  $ wordtagMap wordtags
                              
-wordtagMap :: [String] -> [((String, String), String)]
-wordtagMap wordtags = map rowToTuple $ map words $ wordtags
+wordtagMap wordtags = Map.fromList $ map rowToTuple $ map words $ wordtags
 
 --creates a tuple ((word, tag), count) from a WORDTAG entry
 rowToTuple wordtag = ((word, tag), count)
